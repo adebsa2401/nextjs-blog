cd5a101acd3e2a7657009a2181cd976324626e47
Ben Sales <adebsa2401@gmail.com>
2023-05-31 02:46:03 +0100

    feat: update project repo link

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,8 +8,8 @@ export default function Footer() {
           <h4>Statically Generated with Next.js.</h4>
         </div>
         <div className={styles.footer__right}>
-          <a className={styles['footer__documentation-link']} href="https://nextjs.org/docs/basic-features/pages">Read Documentation</a>
-          <a className={styles['footer__github-link']} href="https://github.com/vercel/next.js/tree/canary/examples/blog-starter">View on GitHub</a>
+          <a className={styles['footer__documentation-link']} href="https://nextjs.org/docs/basic-features/pages" target="_blank" rel="noreferrer noopener">Read Documentation</a>
+          <a className={styles['footer__github-link']} href="https://github.com/adebsa2401/nextjs-blog" target="_blank" rel="noreferrer noopener">View on GitHub</a>
         </div>
       </div>
     </footer>
--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,7 @@ import styles from './header.module.scss';
 export default function Header() {
   return (
     <header className={styles.header}>
-      <p className={styles.header__text}>The source code for this blog is <a className={styles.header__link} href="https://www.github.com/adebsa2401">available on GitHub</a>.</p>
+      <p className={styles.header__text}>The source code for this blog is <a className={styles.header__link} href="https://github.com/adebsa2401/nextjs-blog" target="_blank" rel="noreferrer noopener">available on GitHub</a>.</p>
     </header>
   )
 }
